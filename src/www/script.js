066974cb6918e66259a680cc69aacff91f603f34
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-10 22:44:31 +0200

    Fifth attempt to Heroku

--- src/www/script.js
+++ src/www/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    const apiRoot = 'http://localhost:8080/v1/task/';
-    // var apiRoot = 'https://damp-bayou-80913.herokuapp.com/v1/task/';
-    const trelloApiRoot = 'http://localhost:8080/v1/trello/';
-    // const trelloApiRoot = 'https://damp-bayou-80913.herokuapp.com/v1/trello/';
+    // const apiRoot = 'http://localhost:8080/v1/task/';
+    var apiRoot = 'https://damp-bayou-80913.herokuapp.com/v1/task/';
+    // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+    const trelloApiRoot = 'https://damp-bayou-80913.herokuapp.com/v1/trello/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
