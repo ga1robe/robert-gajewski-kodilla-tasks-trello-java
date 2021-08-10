d4a4360e14b69f1479885029baf04a6c13957199
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-10 23:06:06 +0200

    Fifth attempt to Heroku

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -16,6 +16,9 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+//        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "https://damp-bayou-80913.herokuapp.com/v1/task/");
+        context.setVariable("button", "Visit website");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
