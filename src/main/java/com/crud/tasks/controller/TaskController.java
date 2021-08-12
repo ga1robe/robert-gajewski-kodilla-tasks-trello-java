7176c5632d42a48ca4c2e159039ff2637b0ebd43
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-12 23:21:28 +0200

    sixth attempt to Heroku

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -54,6 +54,7 @@ public class TaskController {
         return taskMapper.mapToTaskDtoList(tasks);
     }
 
+//    @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/taskId={taskId}")
     @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{taskId}")
     public void deleteTask(@PathVariable Long taskId) {
         dbService.deleteTaskById(taskId);
